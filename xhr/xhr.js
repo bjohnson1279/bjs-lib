0e3cf0c033593b6f18819585c792f42fa0ccb843
Brent Johnson <37778321+bjohnson1279@users.noreply.github.com>
2019-11-08 21:52:51 -0700

    More debugging logs

--- xhr/xhr.js
+++ xhr/xhr.js
@@ -60,7 +60,9 @@ export default class XHR extends XMLHttpRequest {
     serializeFormData() {
         console.log('entries', typeof this._formData.entries);
         if (typeof this._formData === 'object' && typeof this._formData.entries === 'function') {
+            console.log('inside if');
             for (const entry of this._formData.entries()) {
+                console.log({ entry });
                 this._params[entry[0]] = entry[1];
             }
         }
